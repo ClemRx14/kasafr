aae9d8626eff22272c382dab938bec359074b708
Clement Raux <ilesquafr@gmail.com>
2024-09-18 18:01:29 +0200

    affichage logemets manque l'intégration à faire

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './Composants/Banner';
 import Card from './Composants/Card';
 
+// Tableau d'objets où chaque objet représente un logement qui se nomme logement
+
 import logementsData from './logements.json';
 
 function Home() {
@@ -11,7 +13,10 @@ function Home() {
         <Banner titre="Chez vous, partout et ailleurs" className="bannierehome" />
       </div>
       <div className='containerCardLogements'>
-
+        {/* Fonction qui reprend logement  */}
+      {logementsData.map((logement) => (
+          <Card key={logement.id} logement={logement} />
+        ))}
       </div>
     </section>
   );
