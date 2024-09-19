9eafbec174ee287da05c18cbd752424d330fffa7
Clement Raux <ilesquafr@gmail.com>
2024-09-19 16:28:08 +0200

    correction lien et route pour logement

--- src/Composants/Card.js
+++ src/Composants/Card.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logement from '../Logement';
 
 
 function Card({ logement }) {
     return (
         <div className='card'>
-            <Link to={`logements/${logement.id}`}>
+            <Link to={`Logement/${logement.id}`}>
             <img src={logement.cover} alt={logement.title} className='imgCard' />
             <h2 className='titleCard'>{logement.title}</h2>
             </Link>
