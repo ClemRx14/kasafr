b234834b216ccba6e5eeaee303c2eaaedc95868b
Clement Raux <ilesquafr@gmail.com>
2024-10-01 19:49:23 +0200

    la page d'erreur est intégrée et fonctionnelle

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
-    <h1 className='errorPageTitle'>404</h1>
+    <div className='containerError'>
+          <h1 className='errorPageTitle'>404</h1>
+          <p className='textePageError'>Oups ! La page que vous demandez n'existe pas.</p>
+          <Link to="/" className='lienHomepage'>Retourner sur la page d'accueil</Link>
+    </div>
  );
 }
 
